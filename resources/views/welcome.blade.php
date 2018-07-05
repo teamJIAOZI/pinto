01984e60e8c9337223e435343a152e12128f0fe7
hiromi.masuda <hiromi.c.masuda@rakuten.com>
2018-07-05 01:28:42 +0000

    first task

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
+     @if (Auth::check())
+        <?php $user = Auth::user(); ?>
+        {{ $user->name }}
+    @else
+
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to pinto</h1>
         </div>
     </div>
+    @endif
 @endsection
