6c71945f1259c943cd41eec91d73e055323cbddf
hiromi.masuda <hiromi.c.masuda@rakuten.com>
2018-07-06 03:44:29 +0000

    data

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,14 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Item;
+
 
 class UsersController extends Controller
 {
      public function menindex()
     {
-        
+        //$item =  Item::where('gender', 1);
+         $item = \DB::table('items')->where('gender', '1')->take(10)->get();
 
         return view('users.menindex', [
+            'items' => $item,
             
         ]);
     }
--- resources/views/users/menindex.blade.php
+++ resources/views/users/menindex.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    
+     
+    <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>アイテム</th>
+                    <th>ストーリー</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($items as $item)
+            <tr>
+                
+                <td>{{ $item->items }}</td>
+                <td>{{ $item->story }}</td>
+            </tr>
+                @endforeach
+            </tbody>
+        </table>        
+                    
+
 @endsection
