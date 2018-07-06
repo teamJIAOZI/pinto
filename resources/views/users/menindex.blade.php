5a7104ae73e1f69694caaec0fbee26317c454fd3
hiromi.masuda <hiromi.c.masuda@rakuten.com>
2018-07-06 06:58:02 +0000

    favorite

--- resources/views/users/menindex.blade.php
+++ resources/views/users/menindex.blade.php
@@ -6,14 +6,16 @@
             <thead>
                 <tr>
                     <th>アイテム</th>
+                    <th>相場</th>
                     <th>ストーリー</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($items as $item)
             <tr>
-                
+                button
                 <td>{{ $item->items }}</td>
+                <td>￥{{ $item->price }}</td>
                 <td>{{ $item->story }}</td>
             </tr>
                 @endforeach
--- resources/views/users/womenindex.blade.php
+++ resources/views/users/womenindex.blade.php
@@ -1,5 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    
+     
+    <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>アイテム</th>
+                     <th>相場</th>
+                    <th>ストーリー</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($items as $item)
+            <tr>
+                
+                <td>{{ $item->items }}</td>
+                <td>￥{{ $item->price }}</td>
+                <td>{{ $item->story }}</td>
+            </tr>
+                @endforeach
+            </tbody>
+        </table>        
+                    
+
 @endsection
