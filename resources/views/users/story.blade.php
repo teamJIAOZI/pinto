eb1c8f898063d3fb6f68edd27b6cec7754f73c5d
hiromi.masuda <hiromi.c.masuda@rakuten.com>
2018-07-09 03:13:19 +0000

    bootstrap

---
+++ resources/views/users/story.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+   <div class="row">
+           <div id = "nav" class="item">
+               <ul>
+                   <li>
+                           <img src="{{ secure_asset("images/coffee.jpg") }}" alt="story">
+                           
+                           <p>hello</p>
+
+                   </li>
+                   <li>
+                           <img src="{{ secure_asset("images/coffee.jpg") }}" alt="story">
+                           <p>good afternoon</p>
+                   </li>
+               </ul>
+           </div>
+   </div>
