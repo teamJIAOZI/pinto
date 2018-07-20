b1beff6ce51051d8329452a32bf6b46a6b8af3a7
hiromi.masuda <hiromi.c.masuda@rakuten.com>
2018-07-20 08:13:35 +0000

    aaaaa

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
         @foreach ($items as $key => $item)
-        <div class = "item">
+        <div class = "item" id="xx_{{$item->id}}">
                 <div class="col-md-3 col-sm-4 col-xs-12">
                        <div id = "nav">
                            <ul>
@@ -30,3 +30,31 @@
               
         @endforeach
 </div>
+<script>
+window.onload = function() {
+var huga = 0;
+var hoge = setInterval(function() {
+    console.log(huga);
+    huga++;
+    //終了条件
+    if (huga == 10) {
+    clearInterval(hoge);
+  console.log('onload 1');
+  var offset = $('#xx_148').offset() || {top: 0, left: 0};
+//  console.log(offset);
+$(window).scrollTop(offset.top);
+
+    console.log("終わり");
+
+    }
+}, 1000);
+
+/*
+    var element = document.getElementById('xx_148');
+    console.log(element);
+    // 上端に来るようにスクロールさせたい
+//    element.scrollIntoView()
+    element.scrollIntoView(false);
+  */  
+}
+</script>
