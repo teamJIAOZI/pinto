9551ee72d2dd1d84f291d461831506a801af5248
hiromi.masuda <hiromi.c.masuda@rakuten.com>
2018-07-20 07:44:49 +0000

    aaa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,7 +41,7 @@
                                   <a href="{{ route('users.searchindex', ['keyword' => $item->item_brand,'keywords' => $item->items] ) }}" class="cp_btnwomen">{{ $key+1 }}位: {{ $item->count}} Favorite </br>
                                   {{ $item->items }}</br>相場:￥{{ $item->price }}</a>
                                   
-                                  @include('user_favorite.favorite_button', ['user' => $user])
+                                  @include('user_favorite.favorite_button', ['user' => $user, 'item' => $item])
 
                                </li>
                            </ul>
@@ -72,7 +72,7 @@
                                   <a href="{{ route('users.searchindex', ['keyword' => $itemgirl->item_brand,'keywords' => $itemgirl->items] ) }}" class="cp_btnwomen">{{ $key+1 }}位: {{ $itemgirl->count}} Favorite </br>
                                   {{ $itemgirl->items }}</br>相場:￥{{ $itemgirl->price }}</a>
                                   
-                                  @include('user_favorite.favorite_button', ['user' => $user])
+                                  @include('user_favorite.favorite_button', ['user' => $user, 'item' => $item])
                                   
                             
                             
